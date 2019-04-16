9cd73d6344a5f56ad6c028b0ba925523a3ba113b
Peter Vican <vican.peter@gmail.com>
2019-04-16 23:05:16 +0200

    add register path

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import {Router, Route, Switch} from 'react-router-dom'
+import {createBrowserHistory} from 'history'
 import styled from '@emotion/styled'
 import LoginContainer from "../containers/LoginContainer";
 import TopMenu from "../containers/TopMenu"
@@ -9,18 +9,18 @@ import * as TM from "../constants/TopMenuConstants"
 import "babel-polyfill";
 
 const Container = styled.div`
-  text-align: center;
-`
-export const history = createBrowserHistory()
+  text-align: center;`;
+export const history = createBrowserHistory();
 
 function Routes() {
   return (
     <Router history={history}>
       <Container>
         <Switch>
-          <Route exact path="/" render={() => <TopMenu />}/>
-          <Route path="/siteA" render={() => <TopMenu menu_items={TM.SITE1} />}/>
-          <Route path="/siteB" render={() => <TopMenu menu_items={TM.SITE2} />}/>
+          <Route exact path="/" render={() => <TopMenu/>}/>
+          <Route path="/siteA" render={() => <TopMenu menu_items={TM.SITE1}/>}/>
+          <Route path="/siteB" render={() => <TopMenu menu_items={TM.SITE2}/>}/>
+          <Route path="/register"/>
         </Switch>
         <Switch>
           <Route exact path="/" component={LoginContainer} />
