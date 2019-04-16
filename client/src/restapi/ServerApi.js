2033c84b297c3894284bbdd518328185c42c2483
Peter Vican <vican.peter@gmail.com>
2019-04-16 23:07:46 +0200

    export register url to api

--- client/src/restapi/ServerApi.js
+++ client/src/restapi/ServerApi.js
@@ -2,3 +2,4 @@ const BASE_URL = "http://localhost:8080/api"
 
 export const LOGIN_URL = BASE_URL + "/login";
 export const EMPLOYEES_URL = BASE_URL + "/employee";
+export const REGISTER_URL = BASE_URL + "/register";
