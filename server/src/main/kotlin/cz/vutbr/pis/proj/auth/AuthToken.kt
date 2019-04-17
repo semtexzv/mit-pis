8eae085b946453db56a2d6c59d96787499b7b97b
Michal Hornický <semtexzv@gmail.com>
2019-04-17 10:51:06 +0200

    Change user modification ,user roles

--- server/src/main/kotlin/cz/vutbr/pis/proj/auth/AuthToken.kt
+++ server/src/main/kotlin/cz/vutbr/pis/proj/auth/AuthToken.kt
@@ -2,14 +2,19 @@ package cz.vutbr.pis.proj.auth
 
 import cz.vutbr.pis.proj.data.Employee
 import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
 
-class AuthToken(val token: String, val user: Employee? = null) : AbstractAuthenticationToken(if (user != null) arrayListOf(user.sysRole) else null) {
+class GrantedAuthorityImpl(val a: String) : GrantedAuthority {
+    override fun getAuthority(): String = a
+}
+
+class AuthToken(val token: String, val user: Employee? = null) : AbstractAuthenticationToken(if (user != null) user.sysRole.authorities.map { GrantedAuthorityImpl(it) } else null) {
     override fun getCredentials(): Any {
         return ""
     }
 
     override fun getPrincipal(): Any {
-        return ""
+        return user ?: Any()
     }
 
 }
--- server/src/main/kotlin/cz/vutbr/pis/proj/auth/CustomSecurityService.kt
+++ server/src/main/kotlin/cz/vutbr/pis/proj/auth/CustomSecurityService.kt
@@ -1,11 +1,17 @@
 package cz.vutbr.pis.proj.auth
 
+import cz.vutbr.pis.proj.repo.EmployeeRepo
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
 @Component
 class CustomSecurityService {
+
+    @Autowired
+    lateinit var employeeRepo: EmployeeRepo
+
     fun currentToken(): AuthToken {
         val token2 = SecurityContextHolder.getContext().authentication;
         val token = token2 as? AuthToken ?: throw BadCredentialsException("Not authorized for this")
@@ -13,7 +19,11 @@ class CustomSecurityService {
     }
 
     fun canModifyUser(id: Int?): Boolean {
-        return true //  currentToken().user?.id == id
+        val token = currentToken();
+
+        return id?.let { id ->
+            (token.user?.sysRole?.ordinal ?: 0) >= (employeeRepo?.getOne(id)?.sysRole?.ordinal ?: 10)
+        } ?: false || currentToken().user?.id == id
     }
 
 }
