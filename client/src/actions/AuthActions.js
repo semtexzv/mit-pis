7b64aa8a3a4db0f89f20a52267d5b11f951b6eb7
Jan Pavlica <jan.pavlica@morosystems.cz>
2019-04-19 11:16:07 +0200

    User set

--- client/src/actions/AuthActions.js
+++ client/src/actions/AuthActions.js
@@ -1,8 +1,14 @@
 import {NAME} from "../constants/AuthConstants";
 
 export const SET_AUTH = `${NAME}/SET_AUTH`;
+export const SET_USER = `${NAME}/SET_USER`;
 
 export const setAuth = (token) => ({
   type: SET_AUTH,
   payload: token,
+});
+
+export const setUser = (user) => ({
+  type: SET_USER,
+  payload: user,
 });
