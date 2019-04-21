3f82d711d08f48e112f14dc1233a5608a7a3ccc5
Peter Vican <vican.peter@gmail.com>
2019-04-21 19:36:45 +0200

    - add endpoint for get brands

--- server/src/main/kotlin/cz/vutbr/pis/proj/repo/SpecializationRepo.kt
+++ server/src/main/kotlin/cz/vutbr/pis/proj/repo/SpecializationRepo.kt
@@ -4,4 +4,9 @@ import cz.vutbr.pis.proj.data.Specialization
 import cz.vutbr.pis.proj.data.SpecializationId
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SpecializationRepo : JpaRepository<Specialization,SpecializationId> {}
+interface SpecializationRepo : JpaRepository<Specialization,SpecializationId> {
+
+
+
+
+}
