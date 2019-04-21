234ab3c0aad350d297ef846d60eb30b40e3b3b14
Peter Vican <vican.peter@gmail.com>
2019-04-21 20:50:16 +0200

    - cleanUp code

--- server/src/main/kotlin/cz/vutbr/pis/proj/repo/SpecializationRepo.kt
+++ server/src/main/kotlin/cz/vutbr/pis/proj/repo/SpecializationRepo.kt
@@ -1,8 +1,5 @@
 package cz.vutbr.pis.proj.repo
 
 import cz.vutbr.pis.proj.data.Specialization
-import org.springframework.data.jpa.repository.JpaRepository
 
-interface SpecializationRepo: BaseSpecialization<Specialization, Specialization> {
-
-}
+interface SpecializationRepo: BaseSpecialization<Specialization, Specialization> 
--- server/src/main/kotlin/cz/vutbr/pis/proj/rest/types/SpecializationData.kt
+++ server/src/main/kotlin/cz/vutbr/pis/proj/rest/types/SpecializationData.kt
@@ -1,5 +1,3 @@
 package cz.vutbr.pis.proj.rest.types
 
-import cz.vutbr.pis.proj.data.SpecializationId
-
 class SpecializationData(val employeeId: Int, val brandId: Int)
