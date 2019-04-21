8685926404d3589606510d1ca826351200354a01
Peter Vican <vican.peter@gmail.com>
2019-04-21 21:22:03 +0200

    - add list for specialization

--- server/src/main/kotlin/cz/vutbr/pis/proj/rest/SpecializationController.kt
+++ server/src/main/kotlin/cz/vutbr/pis/proj/rest/SpecializationController.kt
@@ -8,11 +8,13 @@ import cz.vutbr.pis.proj.repo.SpecializationRepo
 import cz.vutbr.pis.proj.rest.base.BaseSpecializationController
 import cz.vutbr.pis.proj.rest.types.ErrorResponse
 import cz.vutbr.pis.proj.rest.types.SpecializationData
+import cz.vutbr.pis.proj.rest.types.SpecializationDataSingle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
 
 @RestController
 @RequestMapping("/api/specialization")
@@ -24,21 +26,49 @@ class SpecializationController: BaseSpecializationController<Specialization, Spe
     @Autowired
     lateinit var brandRepo: BrandRepo
 
-    @PostMapping("/add")
-    fun addSpecialization(@RequestBody data: SpecializationData?): Any {
+    @PostMapping("/addList")
+    fun addSpecializationList(@RequestBody data: SpecializationData?): Any {
 
       data?.let {
          val employee = employeeRepo.getOne(data.employeeId)
-          val brand = brandRepo.getOne(data.brandId)
-          val specialization = Specialization(SpecializationId(data.employeeId, data.brandId), employee, brand )
 
-          repo.save(specialization)
+          data.brandId.forEach { brandId ->
+              val specializationId = SpecializationId(data.employeeId, brandId)
 
-          return repo.getOne(specialization.id)
+              try {
+                  repo.getOne(specializationId)
+              } catch (ex: Exception){
+                  val brand = brandRepo.getOne(brandId)
+                  repo.save(Specialization(specializationId, employee, brand )
+                  )
+              }
+          }
+
+          return repo.findAllProjectedBy()
+
+
+        } ?: return ErrorResponse("No data")
+    }
+
+    @PostMapping("/add")
+    fun addSpecialization(@RequestBody data: SpecializationDataSingle?): Any {
+
+        data?.let {
+            val employee = employeeRepo.getOne(data.employeeId)
+            val brand = brandRepo.getOne(data.brandId)
+            val specialization = Specialization(SpecializationId(data.employeeId, data.brandId), employee, brand )
+
+            repo.save(specialization)
+
+            return repo.getOne(specialization.id)
         } ?: return ErrorResponse("No data")
 
 
     }
+
+
+
+
 
 
 }
---
+++ server/src/main/kotlin/cz/vutbr/pis/proj/rest/types/SpecializationDataSingle.kt
@@ -0,0 +1,3 @@
+package cz.vutbr.pis.proj.rest.types
+
+class SpecializationDataSingle(val employeeId: Int, val brandId: Int)
