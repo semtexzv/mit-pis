68b06dc9304a7c588c1ca1885bcea35a9f821289
Jan Pavlica <jan.pavlica@morosystems.cz>
2019-04-22 12:05:34 +0200

    TOP MENU acc to ROLE

--- client/src/constants/TopMenuConstants.js
+++ client/src/constants/TopMenuConstants.js
@@ -1,5 +1,36 @@
 // ["nameOfItem", "pathAfterClick"]
 
+import history from '../utils/history'
+
+export const USER = [
+  ["Meetings", "/meeting", "fa-compass"],
+  ["Customers", "/customer", "fa-compass"],
+];
+
+export const MANAGER = [
+  ["Meetings", "/meeting", "fa-compass"],
+  ["Customers", "/customer", "fa-compass"],
+  ["Specializations", "/specialization", "fa-compass"],
+  ["Assign customer", "/connectEmployee", "fa-compass"],
+];
+
+export const ADMIN = [
+  ["User registration", "/register", "pi pi-fw pi-user"],
+  ["Profile", "/profile", "fa-compass"],
+  ["Employees", "/employee", "fa-compass"],
+];
+
+export const OWNER = [
+  ["Meetings", "/meeting", "fa-compass"],
+  ["Specializations", "/specialization", "fa-compass"],
+  ["Assign customer", "/connectEmployee", "fa-compass"],
+  ["User registration", "/register", "pi pi-fw pi-user"],
+  ["Overview", "/overview", "fa-compass"],
+  ["Customers", "/customer", "fa-compass"],
+  ["Profile", "/profile", "fa-compass"],
+  ["Employees", "/employee", "fa-compass"],
+];
+
 export const SITE1 = [
   ["Meetings", "/meeting", "fa-compass"],
   ["Specializations", "/specialization", "fa-compass"],
@@ -10,3 +41,52 @@ export const SITE1 = [
   ["My profile", "/profile", "fa-compass"],
   ["Employees", "/employee", "fa-compass"],
 ];
+
+export function getItemList(role){
+
+  let itemsParam;
+
+  switch (role){
+    case "ADMIN":{
+      itemsParam = ADMIN;
+      break;
+    }
+    case "USER":{
+      itemsParam = USER;
+      break;
+    }
+    case "MANAGER":{
+      itemsParam = MANAGER;
+      break;
+    }
+    case "OWNER":{
+      itemsParam = OWNER;
+      break;
+    }
+    default:
+      itemsParam = SITE1;
+      break;
+  }
+
+
+  var itemList = [];
+  for (let i in itemsParam) {
+    let name = itemsParam[i][0];
+    let path = itemsParam[i][1];
+    let iconText = itemsParam[i][2];
+
+    itemList.push({
+      label: name, icon: iconText, command: () => {
+        history.push(path)
+      }
+    });
+  }
+
+  itemList.push({
+    label: 'Logout', icon: "pi pi-power-off", style: {left: 40}, command: () => {
+      ; // TODO: no action for logout-click not implemented yet
+    }
+  });
+
+  return itemList;
+}
--- client/src/containers/TopMenu.js
+++ client/src/containers/TopMenu.js
@@ -2,56 +2,29 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 import {TabMenu} from "primereact/tabmenu"
-import history from '../utils/history'
+import {getLogged, getMyRole} from "../selectors/AuthSelector";
+import {getItemList} from "../constants/TopMenuConstants";
 
-class TopMenu extends Component {
-  constructor(props) {
-    super(props);
-    if (typeof this.props.menu_items === 'undefined') {
-      this.state = {items: []};
-      return;
-    } else if (this.props.menu_items.length === 0) {
-      this.state = {items: []};
-      return;
-    }
-    //expected menu_items as [["nameOfItem", "pathAfterClick"], ...]
-    // look at ../constants/TopMenuConstants.js
-    var itemsParam = this.props.menu_items;
-    var itemList = [];
-    for (let i in itemsParam) {
-      let name = itemsParam[i][0];
-      let path = itemsParam[i][1];
-      let iconText = itemsParam[i][2];
-
-      itemList.push({
-        label: name, icon: iconText, command: () => {
-          history.push(path)
-
-        }
-      });
-    }
-
-    itemList.push({
-      label: 'Logout', icon: "pi pi-power-off", style: {left: 40}, command: () => {
-        ; // TODO: no action for logout-click not implemented yet
-      }
-    });
-    this.state = {items: itemList};
+const TopMenu = ({
+                   items,
+                   logged
+               }) => {
+  if(logged){
+    return ( <div className="TopMenu">
+        <TabMenu model={items}/>
+      </div> )
+  }else{
+    return (<div></div>);
   }
-
-  render() {
-    return (
-      <div className="TopMenu">
-          <TabMenu model={this.state.items}/>
-      </div>
-    );
-  }
-}
+};
 
 
 
-const mapStateToProps = (menu_state) => ({});
+const mapStateToProps = (state) => ({
+  items: getItemList(getMyRole(state)),
+  logged: getLogged(state),
+});
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(TopMenu);
