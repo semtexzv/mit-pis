53e8e9dfeee92599882183c16ce00345553e0d9b
Michal Hornický <semtexzv@gmail.com>
2019-04-22 19:33:17 +0200

    Remove mpr-fr

--- mpr-frontend/build.cmd
+++
@@ -1,4 +0,0 @@
-if exist "deploy\dist" rmdir deploy\dist /S /Q;
-SET NODE_ENV=production;
-webpack -p --config=webpack.config.prod.js
-SET NODE_ENV=dev;
--- mpr-frontend/src/helpers.js
+++
@@ -1,9 +0,0 @@
-export function getFullName(objectWithNameAndSurname, defaultName = '') {
-  if (!objectWithNameAndSurname) return defaultName
-  const {name, surname} = objectWithNameAndSurname
-  if (name && surname) {
-    return name + ' ' + surname
-  } else {
-    return name || surname || defaultName
-  }
-}
