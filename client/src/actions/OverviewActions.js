897996146058c79d9649acce769dfe6967bfd7c5
Jan Pavlica <jan.pavlica@morosystems.cz>
2019-04-22 10:49:18 +0200

    overview fe connected to be

--- client/src/actions/OverviewActions.js
+++ client/src/actions/OverviewActions.js
@@ -1,1 +1,14 @@
-//import {NAME} from "../constants/OverviewConstants";
+import {NAME} from "../constants/OverviewConstants";
+
+export const INIT_OVERVIEW = `${NAME}/INIT_OVERVIEW`;
+export const UPDATE_OVERVIEW_DATA = `${NAME}/UPDATE_OVERVIEW_DATA`;
+
+
+export const initOverview = () => ({
+  type: INIT_OVERVIEW,
+});
+
+export const updateOverviewData = (rows) => ({
+  type: UPDATE_OVERVIEW_DATA,
+  payload: rows
+});
